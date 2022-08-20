0079687c86879ea4f89b193a83200cb1b2d800f2
Mutuma Gitonga <mgitonga150@gmail.com>
2022-08-20 22:46:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+
+const findMatching = (driver, string) => {
+  return driver.filter(x => {
+    return x.toLowerCase() === string.toLowerCase();
+  });
+}
+
+
+const fuzzyMatch = (driver, string) => {
+  return driver.filter(x => {
+    return (
+      x.startsWith(string) &&
+      x.charAt(x.length-1) !== string &&
+      !x.substring(1, x.length-1).includes(string)
+    );
+  });
+} 
+
+
+const matchName = (driver, string) => {
+  return driver.filter(x => {
+    return x.name === string;
+  });
+}
